389da35c41f6a69040675a8ee3d37a98628909e9
672654 <henrik_krane@hotmail.com>
2025-04-09 16:29:16 +0200

    lokal database er satt opp. Alle tre prosjekter har nå felles database

---
+++ ServiceAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Booking.Data;
+using Microsoft.EntityFrameworkCore;
+using SharedModels.Models;
+
+namespace ServiceAppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BookingContext context;
+        public MainWindow()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BookingContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BookingContext-4b688890-71eb-4e92-936e-455d29dedbb6;Trusted_Connection=True;");
+
+            context = new BookingContext(optionsBuilder.Options);
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var r = context.Reservation.ToList();
+
+            MessageBox.Show($"Antall reservasjoner: {r.Count}");
+
+
+        }
+    }
+}
